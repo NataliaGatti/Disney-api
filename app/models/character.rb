cf9e00486a6dfff3e79547e9f2a03958ee070f17
Natalia Gatti <gattinati.ng@gmail.com>
2022-06-21 15:08:11 -0300

    Added some models validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,6 @@ class Character < ApplicationRecord
   has_one :character_by_movie
   has_many :movie_series, through: :character_by_movie
   has_one_attached :photo
+  validates :name, :age, :weight, :story, presence: { message: "must be given please" }
+  validates :story, length: { minimum: 10 }
 end
--- app/models/movie_serie.rb
+++ app/models/movie_serie.rb
@@ -3,4 +3,7 @@ class MovieSerie < ApplicationRecord
   has_one :character_by_movie
   has_many :characters, through: :character_by_movie
   has_one_attached :photo
+  validates :title, :creation_date, :calification, presence: true
+  validates :calification, inclusion: { in: (1..5), message: "Califique en el rango de 1 a 5." }, _default: 1
+  validates :calification, numericality: true
 end
